0257da40a165253af869106474afa0a886a5ef06
yuliyap-courses <yufiya@yandex.ru>
2020-04-04 15:28:00 +0300

    Human Class2

--- Human.java
+++ Human.java
@@ -12,6 +12,33 @@ public class Human {
     }
 }
 
+class Person extends Human{
+    String name;
+    String surname;
+
+    public Person(int age, String name, String surname) {
+        super(age);
+        this.name = name;
+        this.surname = surname;
+    }
+
+}
+
+class Engineer extends Person {
+    public void inviteTest() {
+        System.out.println(name + "Write code!");
+    }
+
+    public void executeCode() {
+        System.out.println(name + "Execute code!");
+    }
+
+    public Engineer(int age, String name, String surname)
+    {
+        super(age, name, surname);
+    }
+}
+
 
 
 
