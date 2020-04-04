ff4bfdf9269c0bf48010dceeccc6d0ce7558e50d
yuliyap-courses <yufiya@yandex.ru>
2020-04-04 14:57:18 +0300

    Day 1 HomeTask_2

---
+++ Day1HomeTask_2.java
@@ -0,0 +1,12 @@
+package HomeTask.HomeTask;
+
+
+public class Day1HomeTask_2 {
+    public static void main(String[] args) {
+        int f = 0;
+        while (f <= 25) {
+            System.out.print((f++) + " ");
+
+        }
+    }
+}
