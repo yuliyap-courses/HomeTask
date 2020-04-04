8121c39a62c85a6b5c73f0c419b84dc19721169b
yuliyap-courses <yufiya@yandex.ru>
2020-04-04 14:59:24 +0300

    Day 1 HomeTask3

---
+++ Day1HomeTask_3.java
@@ -0,0 +1,25 @@
+package HomeTask.HomeTask;
+
+import java.util.Scanner;
+
+public class Day1HomeTask_3 {
+
+    public static void main (String [] args){
+
+        for (int i=1;i<=20; i++){
+            if (i%2==0){
+                System.out.print(i + " ");
+            }
+        }
+        System.out.print("\n");
+        for (int i=10;i<=20; i++){
+            if (i%2==0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
+
+
+
