570603f3e19d797148a5ac623144b3b855a52abc
yuliyap-courses <yufiya@yandex.ru>
2020-04-04 15:26:45 +0300

    Human Class1

---
+++ Day1HomeTask_5.java
@@ -0,0 +1,27 @@
+package HomeTask.HomeTask;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Day1HomeTask_5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+            System.out.print("Enter the size of the array: ");
+        int a = in.nextInt();
+        int[] arr = new int[a];
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = (int) (Math.random() * a);
+            for (int i : arr) System.out.print(i + " ");
+
+            System.out.print("\n");
+
+            for (int i = 0; i<arr.length /2; i++){
+                int temp = arr[i];
+                arr[i] = arr[arr.length - i -1];
+                arr[arr.length -i - 1] = temp;
+
+            }
+            System.out.print(Arrays.toString(arr));
+        }
+
+    }
---
+++ Human.java
@@ -0,0 +1,17 @@
+package HomeTask.HomeTask;
+
+public class Human {
+    int age;
+
+    public Human(int age) {
+        this.age = age;
+    }
+
+    public void setVoice() {
+        System.out.println("I can speak!");
+    }
+}
+
+
+
+
