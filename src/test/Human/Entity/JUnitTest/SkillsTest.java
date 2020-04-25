caf1b8ed276d1e64b36c2daef78aa703b18864da
yuliyap-courses <yufiya@yandex.ru>
2020-04-25 12:17:52 +0300

    Maven was added to project JUnit dependencies were configured

--- src/test/Human/Entity/JUnitTest/SkillsTest.java
+++ src/test/Human/Entity/JUnitTest/SkillsTest.java
@@ -10,6 +10,6 @@ public class SkillsTest {
     public void Skills(){
         assertTrue("Expression is grater than 31", true);
         assertFalse("Expression is grater than 40", false);
-        assertEquals("Expression is grater than 80", false, false);
+        assertEquals("Expression is grater than 81", false, false);
     }
 }
